496090dfa9d422d1b8fabeb3bdb345d77131342f
j0k3r-trabajo <j0k3r.dev.rgl@gmail.com>
2026-02-06 13:56:06 -0300

    correccion del comando para guardar

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,5 +1,7 @@
 -- Guardar con Ctrl + s (Como en la mayoría de editores)
-vim.keymap.set({ 'n', 'i', 'v' }, '<C-s>', '<cmd>write<cr>', { desc = "Guardar archivo", silent = true })
+vim.keymap.set('n', '<C-s>', '<cmd>write<cr>', { desc = "Guardar archivo", silent = true })
+vim.keymap.set('i', '<C-s>', '<Esc><cmd>write<cr>', { desc = "Guardar archivo", silent = true })
+vim.keymap.set('v', '<C-s>', '<Esc><cmd>write<cr>', { desc = "Guardar archivo", silent = true })
 
 -- Scroll rápido del 25% de la pantalla
 vim.keymap.set('n', '<A-j>', '<C-d>', { desc = "Scroll down 25%", silent = true })
