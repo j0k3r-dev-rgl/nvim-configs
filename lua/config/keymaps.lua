7229443d65f46846d3557d6635b7d8feb1fe36c5
j0k3r-home <j0k3r.dev.rgl@gmail.com>
2026-02-05 23:41:48 -0300

    feat: agregar scroll rapido con Alt+J/K y desactivar navegacion hacia directorio padre en nvim-tree

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,6 +1,9 @@
 -- Guardar con Ctrl + s (Como en la mayoría de editores)
 vim.keymap.set({ 'n', 'i', 'v' }, '<C-s>', '<cmd>write<cr>', { desc = "Guardar archivo", silent = true })
 
+-- Scroll rápido del 25% de la pantalla
+vim.keymap.set('n', '<A-j>', '<C-d>', { desc = "Scroll down 25%", silent = true })
+vim.keymap.set('n', '<A-k>', '<C-u>', { desc = "Scroll up 25%", silent = true })
 
 -- Guardar o Espacio + w (Usando tu tecla Leader)
 local wk = require("which-key")
